e970e48cb42eaa5a90c9a018c18fc6d100c88792
worksapce <289582304@qq.com>
2024-01-16 20:58:13 +0800

    fix: fixed up return undefined

--- index.ts
+++ index.ts
@@ -1,4 +1,20 @@
-type AnyType = 'null' | 'undefined' | 'string' | 'boolean' | 'object' | 'array' | 'symbol' | 'date' | 'function' | 'RegExp' | 'Set' | 'number'
+type AnyType =
+  | "null"
+  | "undefined"
+  | "string"
+  | "boolean"
+  | "object"
+  | "array"
+  | "symbol"
+  | "date"
+  | "function"
+  | "regexp"
+  | "set"
+  | "map"
+  | "weakmap"
+  | "weakset"
+  | "bigint"
+  | "number";
 /**
  * @description 检测数据类型
  * @param data 需要判断的类型的数据，可以是任何数据去判断出任何类型
@@ -12,9 +28,8 @@ function typeOfData(data: unknown): AnyType {
       .slice(8, -1)
       .toLowerCase() as AnyType;
   } catch (error) {
-    return 'null';
+    return "null";
   }
-
 }
 interface JsOverload {
   [key: string]: any;
@@ -22,27 +37,34 @@ interface JsOverload {
 /**
  * js 方法重写
  */
-class JsOverload implements JsOverload{
-  olName = '';
-  olFnMap = new Map()
+class JsOverload implements JsOverload {
+  olName = "";
+  olFnMap = new Map();
   constructor(olName: string) {
-    this.olName = olName
+    this.olName = olName;
   }
 
   /**
-   * 
+   *
    * @param customFn 一个回调函数，它接受多样的参数，这个多样参数和types联动，内部实现了一个map，types的作为key，customFn作为value
    * @param types 注意这里必须要跟第一个参数回调函数的里面的参数类型相同，不然没办法输出你的预期结果
    */
   addImpl = (customFn: (...args: any[]) => void, types: AnyType[]) => {
-    this.olFnMap.set(types.join('_'), customFn)
-    this[this.olName] = function(...ovArgs: any[]) {
-      const ovArgsArr = ovArgs.map(arg => typeOfData(arg))
-      const fn = this.olFnMap.get(ovArgsArr.join('_'));
-      fn(...ovArgs)  
-    }
-  }
+    this.olFnMap.set(types.join("_"), customFn);
+    this[this.olName] = function (...ovArgs: any[]) {
+      const ovArgsArr = ovArgs.map((arg) => typeOfData(arg));
+      const fn = this.olFnMap.get(ovArgsArr.join("_"));
+      return fn(...ovArgs);
+    };
+  };
 }
 
-export default JsOverload
+export default JsOverload;
 
+const fn = new JsOverload("testFn");
+const testFnNumNum = (a: number, b: number) => {
+  return a + b;
+};
+const testFnStrStr = (a: string, b: string) => {
+  return [a, b].join("___");
+};
